0139131217b8e8f162a20ad123d0e992ff263494
Sajid Khurshid <sajid.khurshid@eviden.com>
2024-03-26 15:56:34 +0000

    Further test changes.

--- src/EPR.Accreditation.Portal/EPR.Accreditation.UnitTests/Services/AccreditationServiceTests.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.UnitTests/Services/AccreditationServiceTests.cs
@@ -1,10 +1,8 @@
-﻿
-using AutoMapper;
+﻿using AutoMapper;
 using EPR.Accreditation.Portal.Options;
 using EPR.Accreditation.Portal.RESTservices.Interfaces;
 using EPR.Accreditation.Portal.Services.Accreditation;
 using EPR.Accreditation.Portal.ViewModels;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Moq;
 
@@ -13,7 +11,7 @@ namespace EPR.Accreditation.UnitTests.Services
     [TestClass]
     public class AccreditationServiceTests
     {
-        private Mock<Mapper> _mapper;
+        private Mock<IMapper> _mockMapper;
         private Mock<IHttpAccreditationService> _httpAccreditionService;
         private AccreditationService _accreditationService;
         private Mock<IOptions<AppSettingsConfigOptions>> _mockConfigSettings;
@@ -26,12 +24,12 @@ namespace EPR.Accreditation.UnitTests.Services
                 DaysUntilExpiration = 30
             };
 
-            _mapper = new Mock<Mapper>();
+            _mockMapper = new Mock<IMapper>();
             _mockConfigSettings = new Mock<IOptions<AppSettingsConfigOptions>>();
             _httpAccreditionService = new Mock<IHttpAccreditationService>();
             _mockConfigSettings.Setup(o => o.Value).Returns(mockConfig);
 
-            _accreditationService = new AccreditationService(_mapper.Object, _mockConfigSettings.Object, _httpAccreditionService.Object);
+            _accreditationService = new AccreditationService(_mockMapper.Object, _mockConfigSettings.Object, _httpAccreditionService.Object);
 
         }
 
