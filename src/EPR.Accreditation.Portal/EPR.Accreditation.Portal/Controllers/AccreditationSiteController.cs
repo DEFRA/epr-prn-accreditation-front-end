a50592f2e2b7be7c12a620cb2465cb91fda1a7d8
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-03 19:26:25 +0100

    [feature/336347: Created Enligh and Welsh resource files + updated view and viewModel]

--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/Controllers/AccreditationSiteController.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/Controllers/AccreditationSiteController.cs
@@ -42,5 +42,26 @@ namespace EPR.Accreditation.Portal.Controllers
 
             return View();
         }
+
+        [HttpPost("ExemptionReferences")]
+        public async Task<IActionResult> ExemptionReferences(
+            Guid? id,
+            Guid? siteId,
+            ExemptionReferencesViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                // If the ModelState is not valid, return the view with validation errors
+                return View(viewModel);
+            }
+
+            // At this point, the model state is valid, and you can access the reference numbers
+            // from viewModel.ReferenceNumber1, viewModel.ReferenceNumber2, etc.
+            // You can also access all reference numbers from viewModel.ExemptionReferences.
+
+            // Here, you can process the data, save it to a database, etc.
+
+            return RedirectToAction("Success");
+        }
     }
 }
--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/ViewModels/ExemptionReferencesViewModel.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/ViewModels/ExemptionReferencesViewModel.cs
@@ -1,22 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EPR.Accreditation.Portal.Resources;
+using System.ComponentModel.DataAnnotations;
 
 namespace EPR.Accreditation.Portal.ViewModels
 {
     public class ExemptionReferencesViewModel
     {
-        [Required(ErrorMessage = "Reference Number 2 is required")]
+        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
+        [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber1 { get; set; }
 
-        [Required(ErrorMessage = "Reference Number 2 is required")]
+        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
+        [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber2 { get; set; }
 
-        [Required(ErrorMessage = "Reference Number 3 is required")]
+        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
+        [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber3 { get; set; }
 
-        [Required(ErrorMessage = "Reference Number 4 is required")]
+        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
+        [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber4 { get; set; }
 
-        [Required(ErrorMessage = "Reference Number 5 is required")]
+        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
+        [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber5 { get; set; }
     }
 }
