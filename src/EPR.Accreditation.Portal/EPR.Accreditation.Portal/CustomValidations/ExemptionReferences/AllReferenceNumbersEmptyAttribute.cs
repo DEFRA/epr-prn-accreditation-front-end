28156e2d687d0251a754119d41758d579937ee17
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-03 19:33:51 +0100

    [feature/336347: Created a custom validation attribute to check at least one reference entered]

---
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/CustomValidations/ExemptionReferences/AllReferenceNumbersEmptyAttribute.cs
@@ -0,0 +1,26 @@
+﻿using EPR.Accreditation.Portal.Resources;
+using EPR.Accreditation.Portal.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace EPR.Accreditation.Portal.CustomValidations.ExemptionReferences
+{
+    public class AllReferenceNumbersEmptyAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var viewModel = (ExemptionReferencesViewModel)validationContext.ObjectInstance;
+
+            // Check if all reference numbers are empty
+            if (string.IsNullOrEmpty(viewModel.ReferenceNumber1) &&
+                string.IsNullOrEmpty(viewModel.ReferenceNumber2) &&
+                string.IsNullOrEmpty(viewModel.ReferenceNumber3) &&
+                string.IsNullOrEmpty(viewModel.ReferenceNumber4) &&
+                string.IsNullOrEmpty(viewModel.ReferenceNumber5))
+            {
+                return new ValidationResult(ErrorMessage ?? ExemptionReferencesResources.ErrorMessageBlank);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/ViewModels/ExemptionReferencesViewModel.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/ViewModels/ExemptionReferencesViewModel.cs
@@ -1,31 +1,32 @@
-﻿using EPR.Accreditation.Portal.Resources;
+﻿using EPR.Accreditation.Portal.CustomValidations.ExemptionReferences;
+using EPR.Accreditation.Portal.Resources;
 using System.ComponentModel.DataAnnotations;
 
 namespace EPR.Accreditation.Portal.ViewModels
 {
     public class ExemptionReferencesViewModel
     {
-        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [AllReferenceNumbersEmpty(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
         [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
         [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber1 { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [AllReferenceNumbersEmpty(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
         [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
         [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber2 { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [AllReferenceNumbersEmpty(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
         [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
         [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber3 { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [AllReferenceNumbersEmpty(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
         [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
         [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber4 { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
+        [AllReferenceNumbersEmpty(ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageBlank")]
         [RegularExpression(@"^[a-zA-Z0-9/]*$", ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageInvalidFormat")]
         [StringLength(20, ErrorMessageResourceType = typeof(ExemptionReferencesResources), ErrorMessageResourceName = "ErrorMessageTooLong")]
         public string ReferenceNumber5 { get; set; }
