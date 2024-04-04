d49385e8150edf491b7330219e3ace76067f15e0
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-04 17:33:01 +0100

    [feature/336342: WIP unit tests]

--- src/EPR.Accreditation.Portal/EPR.Accreditation.UnitTests/Controllers/AccreditationControllerTests.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.UnitTests/Controllers/AccreditationControllerTests.cs
@@ -22,9 +22,19 @@ namespace EPR.Accreditation.UnitTests.Controllers
         [TestInitialize]
         public void Init()
         {
-            _mockAccreditationService = new Mock<IAccreditationService>();
             _mockSaveAndComeBackService = new Mock<ISaveAndComeBackService> { CallBase = true };
-            _accreditationController = new AccreditationController(_mockWastePermitService.Object, _mockSaveAndComeBackService.Object, _mockAccreditationService.Object, _mockUrlHelper.Object, _backPageViewModel.Object);
+            _mockhttpAccreditationService = new Mock<IHttpAccreditationService>();
+            _mockAccreditationService = new Mock<IAccreditationService>();
+            _mockWastePermitService = new Mock<IWastePermitService>();
+            _mockUrlHelper = new Mock<IUrlHelper>();
+            _backPageViewModel = new Mock<BackPageViewModel>();
+
+            _accreditationController = new AccreditationController(
+                _mockWastePermitService.Object,
+                _mockSaveAndComeBackService.Object,
+                _mockAccreditationService.Object,
+                _mockUrlHelper.Object,
+                _backPageViewModel.Object);
         }
 
         [TestMethod]
@@ -53,6 +63,75 @@ namespace EPR.Accreditation.UnitTests.Controllers
             Assert.IsNull(viewResult.ViewName); // It's going to return the view name of the action by default
 
             _mockAccreditationService.Verify(service => service.GetWastePermitViewModel(id), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task CheckWastePermitExemption_ReturnsViewWithViewModel()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var expectedViewModel = new PermitExemptionViewModel();
+            var expectedUrl = "expectedUrl";
+
+            _mockUrlHelper.Setup(helper => helper.ActionLink(
+                "ApplyForAccreditation", "Home", null, null, null, null)).Returns(expectedUrl);
+
+            _mockWastePermitService.Setup(service => service.GetPermitExemptionViewModel(id)).ReturnsAsync(expectedViewModel);
+
+            // Act
+            var result = await _accreditationController.CheckWastePermitExemption(id);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult.ViewData.Model);
+
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(PermitExemptionViewModel));
+
+            Assert.IsNull(viewResult.ViewName);
+
+            _mockWastePermitService.Verify(service => service.GetPermitExemptionViewModel(id), Times.Once());
+
+        }
+
+        [TestMethod]
+        public async Task CheckWastePermitExemption_ReturnsNotFound_WhenIdIsNull()
+        {
+            // Arrange
+            Guid? id = null;
+
+            // Act
+            var result = await _accreditationController.CheckWastePermitExemption(id);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+
+            _mockWastePermitService.Verify(service => service.GetPermitExemptionViewModel((Guid)id), Times.Never());
+
+        }
+
+        [TestMethod]
+        public async Task CheckWastePermitExemption_SetsBackPageViewModelUrl()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var expectedUrl = "expectedUrl";
+
+            _mockUrlHelper.Setup(helper => helper.ActionLink(
+                "ApplyForAccreditation", "Home", null, null, null, null)).Returns(expectedUrl);
+
+            // Act
+            var result = await _accreditationController.CheckWastePermitExemption(id);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.AreEqual(expectedUrl, viewResult.ViewData["Url"]);
+
+            _mockWastePermitService.Verify(service => service.GetPermitExemptionViewModel(id), Times.Once());
+
         }
     }
 }
