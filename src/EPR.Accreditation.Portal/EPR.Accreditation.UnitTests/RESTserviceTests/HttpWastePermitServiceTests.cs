77312a5431e1e67bcf1bb6568fa454590902f745
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-05 18:00:45 +0100

    [fix-unit-tests: WIP HttpWastePermitServiceTests]

---
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.UnitTests/RESTserviceTests/HttpWastePermitServiceTests.cs
@@ -0,0 +1,107 @@
+﻿using EPR.Accreditation.Portal.DTOs.WastePermit;
+using EPR.Accreditation.Portal.RESTservices;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace EPR.Accreditation.UnitTests.RESTserviceTests
+{
+    [TestClass]
+    public class HttpWastePermitServiceTests
+    {
+        protected HttpWastePermitService _httpWastePermitService;
+        protected Mock<IHttpContextAccessor> _contextAccessor;
+        protected Mock<IHttpClientFactory> _httpClientFactory;
+        protected HttpClient _httpClient;
+        protected Mock<DelegatingHandler> _clientHandlerMock;
+        protected string _baseUrl = "http://baseUrl";
+        protected string _endpointName = "endpointName";
+        protected string _capturedUrl;
+        protected string _capturedPayload;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _clientHandlerMock = new Mock<DelegatingHandler>();
+
+            _clientHandlerMock.As<IDisposable>().Setup(s => s.Dispose());
+            _httpClient = new HttpClient(_clientHandlerMock.Object);
+
+            _contextAccessor = new Mock<IHttpContextAccessor>();
+            _httpClientFactory = new Mock<IHttpClientFactory>(MockBehavior.Strict);
+
+            _httpClientFactory.Setup(f => f.CreateClient(string.Empty)).Returns(_httpClient).Verifiable();
+            _httpWastePermitService = new HttpWastePermitService(
+                _contextAccessor.Object,
+                _httpClientFactory.Object,
+                _baseUrl,
+                _endpointName);
+
+            SetClientResponse();
+
+        }
+
+        private void SetClientResponse(
+            HttpStatusCode httpStatusCode = HttpStatusCode.OK,
+            object content = null)
+        {
+            var response = new HttpResponseMessage(httpStatusCode);
+
+            if (content != null)
+            {
+                response.Content = new StringContent(JsonConvert.SerializeObject(content));
+            }
+
+            _clientHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .Callback<HttpRequestMessage, CancellationToken>((request, cancellationToken) =>
+                {
+                    _capturedUrl = request.RequestUri.ToString().TrimEnd('/');
+                    _capturedPayload = request.Content?.ReadAsStringAsync().Result; // Read the content as string
+                })
+                .ReturnsAsync(response)
+                .Verifiable();
+        }
+
+        [TestMethod]
+        public async Task GetHasPermitExemption_CallsEndpointSuccesfully()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            SetClientResponse(HttpStatusCode.OK);
+            var expectedUrl = $"{_baseUrl}/{_endpointName}/{id}/WastePermitExemption";
+            var hasPermitExemption = true;
+            SetClientResponse(HttpStatusCode.OK, hasPermitExemption);
+
+            // Act
+            var result = await _httpWastePermitService.GetHasPermitExemption(id);
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(expectedUrl.ToLower(), _capturedUrl.ToLower());
+        }
+
+        [TestMethod]
+        public async Task UpdateWasteSource_ForSite_CallsEndpointSuccesfully_WithExpectedPayload()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var hasExemptionReference = new PermitExemption { HasPermitExemption = false };
+            SetClientResponse(HttpStatusCode.OK);
+            var expectedUrl = $"{_baseUrl}/{_endpointName}/{id}/WastePermitExemption";
+
+            // Act
+            await _httpWastePermitService.UpdatePermitExemption(
+                id,
+                hasExemptionReference);
+
+            // Assert
+            _capturedPayload = JsonConvert.DeserializeObject<string>(_capturedPayload); // comes back as json, so need to deserialize
+            Assert.AreEqual(expectedUrl.ToLower(), _capturedUrl.ToLower());
+            Assert.AreEqual(hasExemptionReference, _capturedPayload);
+        }
+    }
+}
