f90e641da84afedf762d207b14c52962798fe6e1
Nicholas Peters <nicholas.peters.external@eviden.com>
2024-04-05 14:37:19 +0100

    Renamed the resources for the views

--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/Resources/MaterialOutputsEstimatedResources.Designer.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/Resources/MaterialOutputsEstimatedResources.Designer.cs
@@ -22,14 +22,14 @@ namespace EPR.Accreditation.Portal.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class MaterialOutputsResources {
+    public class MaterialOutputsEstimatedResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal MaterialOutputsResources() {
+        internal MaterialOutputsEstimatedResources() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace EPR.Accreditation.Portal.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EPR.Accreditation.Portal.Resources.MaterialOutputsResources", typeof(MaterialOutputsResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EPR.Accreditation.Portal.Resources.MaterialOutputsEstimatedResources", typeof(MaterialOutputsEstimatedResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -70,7 +70,7 @@ namespace EPR.Accreditation.Portal.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Contaminents.
+        ///   Looks up a localized string similar to Contaminants.
         /// </summary>
         public static string ContaminentsField {
             get {
@@ -124,7 +124,7 @@ namespace EPR.Accreditation.Portal.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Process Loss.
+        ///   Looks up a localized string similar to Process loss.
         /// </summary>
         public static string ProcessLossField {
             get {
@@ -142,7 +142,7 @@ namespace EPR.Accreditation.Portal.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enter your waste material outputs for the last calendar year.
+        ///   Looks up a localized string similar to Estimate your annual waste outputs.
         /// </summary>
         public static string Title {
             get {
