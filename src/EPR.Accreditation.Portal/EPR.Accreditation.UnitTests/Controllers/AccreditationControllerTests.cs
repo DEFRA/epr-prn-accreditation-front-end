6a9c526617cce6e2ded26eaf0278e62f58fde1f0
Sajid Khurshid <sajid.khurshid@eviden.com>
2024-04-05 14:47:20 +0100

    Align with dev

--- src/EPR.Accreditation.Portal/EPR.Accreditation.UnitTests/Controllers/AccreditationControllerTests.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.UnitTests/Controllers/AccreditationControllerTests.cs
@@ -37,7 +37,6 @@ namespace EPR.Accreditation.UnitTests.Controllers
 
         [Ignore]
         [TestMethod]
-        [Ignore]
         public void WasteLicensesAndPermits_ReturnsCorrectly_WithValidId()
         {
             // Arrange
