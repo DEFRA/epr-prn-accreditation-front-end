86196c7c89106a4ad4449e341e70ca9c9656ebf6
Nicholas Peters <nicholas.peters.external@eviden.com>
2024-04-12 17:02:59 +0100

    Removed suppresion that was useful for build

--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/GlobalSuppressions.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/GlobalSuppressions.cs
@@ -10,5 +10,3 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Another standard Microsoft do not use")]
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1413:Use trailing comma in multi-line initializers", Justification = "This is just unnecessary")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Just adding this to reduce warnings. May have to be addressed at a later date")]
-
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>")]
