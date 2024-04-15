e4d0b96953c6a71a5c7e91507fe3cde307f8d79c
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-15 10:41:11 +0100

    [local-build-fix: Added missing using statement]

--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/RESTservices/HttpAccreditationSiteService.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/RESTservices/HttpAccreditationSiteService.cs
@@ -4,6 +4,7 @@
 
 namespace EPR.Accreditation.Portal.RESTservices
 {
+    using EPR.Accreditation.Facade.Common.RESTservices;
     using EPR.Accreditation.Portal.RESTservices.Interfaces;
 
     public class HttpAccreditationSiteService : BaseHttpService, IHttpAccreditationSiteService
