2f47c1a6d1487cc78971e5e04ef8449217b5f198
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-16 12:30:35 +0100

    [feature/346567: Addressing StyleCop issues]

--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/DTOs/OverseasSite/ReprocessorDetailsDto.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/DTOs/OverseasSite/ReprocessorDetailsDto.cs
@@ -1,16 +1,28 @@
 ﻿namespace EPR.Accreditation.Portal.DTOs.OverseasSite
 {
-    using EPR.Accreditation.Portal.Resources;
     using System.ComponentModel.DataAnnotations;
+    using EPR.Accreditation.Portal.Resources;
 
+    /// <summary>
+    /// DTO to for the view model
+    /// </summary>
     public class ReprocessorDetailsDto
     {
+        /// <summary>
+        /// Gets or sets the organisation name
+        /// </summary>
         [MaxLength(100)]
         public string OrganisationName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the selected country
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(ReprocessorDetailsResources), ErrorMessageResourceName = "ErrorCountry")]
         public string SelectedCountry { get; set; }
 
+        /// <summary>
+        /// Gets or sets the address
+        /// </summary>
         [Required(ErrorMessageResourceType = typeof(ReprocessorDetailsResources), ErrorMessageResourceName = "ErrorAddress")]
         [MaxLength(500)]
         public string Address { get; set; }
