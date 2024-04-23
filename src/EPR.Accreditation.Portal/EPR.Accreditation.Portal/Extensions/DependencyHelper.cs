4f43304507f18843d824eb4ff707dedebe4f67fa
Sajid Khurshid <sajid.khurshid@eviden.com>
2024-04-23 11:12:10 +0100

    Review changes.

--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/Extensions/DependencyHelper.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal/Extensions/DependencyHelper.cs
@@ -103,8 +103,7 @@
                         s.GetRequiredService<IHttpContextAccessor>(),
                         s.GetRequiredService<IHttpClientFactory>(),
                         s.GetRequiredService<IOptions<ServicesConfiguration>>().Value.AccreditationFacade.Url,
-                        "Accreditation"
-                    ));
+                        "Accreditation"));
 
             var mapperConfig = new MapperConfiguration(mc =>
             {
