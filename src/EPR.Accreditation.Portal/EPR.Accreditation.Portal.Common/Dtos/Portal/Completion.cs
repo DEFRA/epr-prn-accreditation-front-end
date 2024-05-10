234656c3caf2b24935b8d37490bb36f7cfabb20f
Sajid Khurshid <sajid.khurshid@eviden.com>
2024-05-10 10:51:57 +0100

    Comments.

--- src/EPR.Accreditation.Portal/EPR.Accreditation.Portal.Common/Dtos/Portal/Completion.cs
+++ src/EPR.Accreditation.Portal/EPR.Accreditation.Portal.Common/Dtos/Portal/Completion.cs
@@ -1,9 +1,18 @@
 ﻿namespace EPR.Accreditation.Portal.Common.Dtos.Portal
 {
+    /// <summary>
+    /// Completion DTO class.
+    /// </summary>
     public class Completion
     {
+        /// <summary>
+        /// Gets or sets the accrediation fee.
+        /// </summary>
         public decimal AccreditationFee { get; set; }
 
+        /// <summary>
+        /// Gets or sets the reference number.
+        /// </summary>
         public string ReferenceNumber { get; set; }
     }
 }
